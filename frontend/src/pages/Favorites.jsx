62fd44df3b1ce639e8ae320e69a9bee48e9fc9ab
gao <gaojingru.cn@gmail.com>
2025-10-28 16:22:20 +0800

    finish basic

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import "../css/Favorites.css"
+
+function Favorites() {
+    return <div className=" favorites-empty">
+        <h2>Your Favorites List is Empty</h2>
+        <p>Start adding movies to your favorites and they will display here.</p>
+    </div>
+}
+
+export default Favorites
